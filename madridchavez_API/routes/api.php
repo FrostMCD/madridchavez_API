1ef4d2a72cc5b98893c9a2c4f71492be1ab7047a
Steve Enedicto Madrid Chavez <Stevemadridufg@gmail.com>
2025-10-23 20:03:39 -0600

    Clase del 23

--- madridchavez_API/routes/api.php
+++ madridchavez_API/routes/api.php
@@ -9,6 +9,7 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-Route::get('/zonas', [ZonaController::class,'obtenerZonas']); //PLURAL
-
+Route::get('/zonas',[ZonaController::class,'obtenerZonas']); //PLURAL
 Route::get('/zona/{idzona}', [ZonaController::class,'obtenerZona']); //SINGULAR
+Route::get('/zonaspais/{idpais}', [ZonaController::class,'obtenerZonaPais']); 
+Route::post('/nuevazona', [ZonaController::class,'crearZona']); 
